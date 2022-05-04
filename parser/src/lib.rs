765da15c9011b44b172f87bd8a62884539f18c06
ThatEpicBanana <thatepicbanana132@gmail.com>
2022-05-03 20:57:08 -0400

    Add option of verbose parsing and formatted Opt

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,9 +1,35 @@
+// temporary
+//TODO: remove this
+#![allow(dead_code)]
+
+use std::fmt::{self, Debug};
+
 use chumsky::{prelude::*, Stream, chain::Chain};
 
 /// A type representing a span of input text 
 pub type Span = std::ops::Range<usize>;
 /// A type representing an optional AST node, with Err holding a [`Span`]
-pub type Opt<T> = Result<T, Span>;
+// pub type Opt<T> = Result<T, Span>;
+
+#[derive(Clone, PartialEq, Eq, Hash)]
+pub enum Opt<T> {
+    Ok(T),
+    Err(Span),
+}
+
+impl<T: Debug> fmt::Debug for Opt<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match &self {
+            Ok(x) => 
+                if f.alternate() { write!(f, "{:#?}", x) } 
+                else { write!(f, "{:?}", x) }
+            Err(x) => 
+                if f.alternate() { write!(f, "Error: {:#?}", x) } 
+                else { write!(f, "Error: {:?}", x) },
+        }
+    }
+}
+
 
 pub mod lexer;
 mod parser;
@@ -19,12 +45,24 @@ pub fn create() -> impl Parser<Token, Vec<Item>, Error = Simple<Token>> {
 }
 
 pub struct ParserOutput {
-    out: Option<Vec<Item>>,
-    lexer_errors: Vec<Simple<char>>,
-    parser_errors: Vec<Simple<Token>>,
+    pub out: Option<Vec<Item>>,
+    pub lexer_errors: Vec<Simple<char>>,
+    pub parser_errors: Vec<Simple<Token>>,
 }
 
 pub fn parse<'a, Iter, S>(input: S) -> ParserOutput 
+where 
+    Iter: Iterator<Item = (char, Span)> + 'a,
+    S: Into<Stream<'a, char, Span, Iter>>,
+{ parse_inner(input, false) }
+
+pub fn parse_verbose<'a, Iter, S>(input: S) -> ParserOutput 
+where 
+    Iter: Iterator<Item = (char, Span)> + 'a,
+    S: Into<Stream<'a, char, Span, Iter>>,
+{ parse_inner(input, true) }
+
+fn parse_inner<'a, Iter, S>(input: S, verbose: bool) -> ParserOutput 
 where 
     Iter: Iterator<Item = (char, Span)> + 'a,
     S: Into<Stream<'a, char, Span, Iter>>,
@@ -33,13 +71,16 @@ where
     let len = input.len();
 
     // try lexing
+    //TODO: make it so this only works in test environments
     let (out, lexer_errors) = 
-        lexer::create().parse_recovery(input);
+        if verbose { lexer::create().parse_recovery_verbose(input) }
+        else       { lexer::create().parse_recovery        (input) };
 
     // check output of lexer
     let (out, parser_errors) = if let Some(out) = out {
         // if lexer succeeds, try parsing
-        crate::create().parse_recovery(Stream::from_iter(len..len + 1, out.into_iter()))
+        if verbose { crate::create().parse_recovery_verbose(Stream::from_iter(len..len + 1, out.into_iter())) }
+        else       { crate::create().parse_recovery        (Stream::from_iter(len..len + 1, out.into_iter()))}
     } else { 
         // if lexer fails, output lexer errors
         return ParserOutput{out: None, lexer_errors, parser_errors: Vec::new()};
